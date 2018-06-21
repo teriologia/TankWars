dc2065f938e6631d5ee978393b3d19d0badc3c1b
LAPTOP-FQO0R4FD\HP <teriologia807@gmail.com>
2018-06-22 00:38:04 +0300

    AI stuff PT.1

---
+++ TankWars/Source/TankWars/Private/TankAIController.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI Can't Find PlayerTank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI found PlayerTank: %s"), *(PlayerTank->GetName()));
+	}
+}
+
+ATank * ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank * ATankAIController::GetPlayerTank() const
+{
+	/*auto PlayerPawn = GetWorld()->GetFirtsPlayerController()->GetPawn;
+	if (!Cast)
+	{
+		return nullptr;
+	}*/
+	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+}
---
+++ TankWars/Source/TankWars/Public/TankAIController.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Tank.h"
+#include "GameFramework/Actor.h"
+#include "TankAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKWARS_API ATankAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void BeginPlay() override;
+
+private:
+	ATank * GetControlledTank() const;
+	ATank * GetPlayerTank() const;
+};
